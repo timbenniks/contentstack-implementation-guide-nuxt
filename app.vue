bde52d0718a5f127d0f6f7d44b50f69681f30efa
Lo Etheridge <lo.etheridge@contentstack.com>
2024-12-20 14:07:59 -0500

    add comments to app.vue

--- app.vue
+++ app.vue
@@ -1,15 +1,19 @@
 <script lang="ts" setup>
+// Import the empty block parent class from Contentstack Live Preview utils
 import VB_EmptyBlockParentClass from "@contentstack/live-preview-utils";
 
+// Fetch the page data and refresh function using the useGetPage composable
 const { data: page, refresh } = await useGetPage("/");
 
 onMounted(() => {
+
   const { $preview, $ContentstackLivePreview } = useNuxtApp();
   $preview && $ContentstackLivePreview.onEntryChange(refresh);
 });
 </script>
 <template>
   <main class="max-w-screen-md mx-auto">
+    <!-- Main container with conditional classes-->
     <section class="p-4">
       <h1
         v-if="page?.title"
@@ -41,28 +45,31 @@ onMounted(() => {
         v-if="page?.rich_text"
         v-bind="page?.$ && page?.$.rich_text"
         v-html="page?.rich_text"
-      />
+    />
 
       <div
         :class="[
-          'space-y-8 max-w-screen-sm mt-4',
-          !page?.blocks || page.blocks.length === 0
-            ? VB_EmptyBlockParentClass
-            : '',
-        ]"
+          'space-y-8 max-w-screen-sm mt-4',  // Base classes for spacing and width
+          !page?.blocks || page.blocks.length === 0 
+            ? VB_EmptyBlockParentClass // Add the empty block parent class if no blocks are present
+            : '', // Otherwise, no additional class
+      ]"
         v-bind="page?.$ && page?.$.blocks"
       >
+
         <div
           v-for="(item, index) in page?.blocks"
           :key="item.block._metadata.uid"
           v-bind="page?.$ && page?.$[`blocks__${index}`]"
           :class="[
+            // Additional classes for each block based on block layout
             'flex flex-col md:flex-row items-center space-y-4 md:space-y-0 md:space-x-4 bg-slate-100',
             item.block.layout === 'image_left'
               ? 'md:flex-row'
               : 'md:flex-row-reverse',
           ]"
         >
+        <!-- Block content-->
           <div class="w-full md:w-1/2">
             <img
               v-if="item.block.image"
